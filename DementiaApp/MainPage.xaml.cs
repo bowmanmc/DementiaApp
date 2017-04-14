8ba508e6e98adeb40bcb2fea157a2c9427c8a8ce
Michael Bowman <bowmanmc@gmail.com>
2017-04-13 20:08:04 -0400

    Add project files.

---
+++ DementiaApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DementiaApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            User u = new DementiaApp.User();
+            Debug.WriteLine("User {0} created at {1}", u.Id, u.DateCreated);
+            u.Name = "Michael Bowman";
+            u.Email = "michael.bowman3005@sinclair.edu";
+
+            Debug.WriteLine(u);
+        }
+    }
+}
---
+++ DementiaApp/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DementiaApp
+{
+    public class User
+    {
+        private Guid _id;
+        private DateTime _dateCreated;
+        private String _name;
+        private String _email;
+
+        public User()
+        {
+            _id = Guid.NewGuid();
+            _dateCreated = DateTime.Now;
+        }
+
+        public String Id
+        {
+            get { return this._id.ToString(); }
+        }
+
+        public DateTime DateCreated
+        {
+            get { return this._dateCreated; }
+        }
+
+        public String Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+
+        public String Email
+        {
+            get { return this._email; }
+            set
+            {
+                if (value.Contains("@") && value.Contains("."))
+                {
+                    this._email = value;
+                }
+                else
+                {
+                    throw new Exception("User email must contain @ and . characters...");
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} <{1}> - {2}", this._name, this._email, this._id);
+        }
+    }
+}
